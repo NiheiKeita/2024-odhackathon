98a64d8ae9add72c406d669c9598bb66a4c59265
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-08-24 15:59:02 +0900

    修正中

--- src/resources/js/Components/Card/index.tsx
+++ src/resources/js/Components/Card/index.tsx
@@ -30,67 +30,62 @@ export const Card = React.memo<Props>(function Card({
 }) {
 
     return (
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full">
+        <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full cursor-pointer" onClick={onJoinClick}>
             <div className="flex">
-                {/* Sport Image */}
-                <div className="w-1/4 relative aspect-w-1 aspect-h-1">
+                <div className="w-2/5 relative aspect-w-1 aspect-h-1 px-4 py-6">
+                    <h2 className="text-lg font-semibold text-center whitespace-nowrap">初心者OK</h2>
                     <img
                         src={sportImage}
                         alt={sportName}
                         className="w-full object-cover"
                     />
-                    <h2 className="text-xl font-semibold mt-2 text-center">{sportName}</h2>
+                    <div>
+                        <div className="flex justify-between text-xs mt-2">
+                            <p className="text-left whitespace-nowrap">日時：</p>
+                            <p className="text-left ml-2 text-ellipsis overflow-hidden">{dateTime}</p>
+                        </div>
+                        <div className="flex justify-between text-xs mt-2">
+                            <p className="text-left whitespace-nowrap">費用：</p>
+                            <p className="text-left ml-2 text-ellipsis overflow-hidden">{fee}</p>
+                        </div>
+                    </div>
+                    {/* <div className='p-2'>
+                        <p className="text-md mt-2 text-left">日時：{dateTime}</p>
+                        <p className="text-md mt-2 text-left">費用：{fee}</p>
+                    </div> */}
                 </div>
 
-                <div className="flex-grow p-6">
-                    {/* Date and Time */}
-                    <p className="text-gray-800 text-2xl font-bold mb-4">{dateTime}</p>
-
-                    {/* Location */}
-                    <p className="text-gray-600 mt-2">
-                        {location} <span className="text-gray-400">({nearestStation})</span>
-                    </p>
-
-                    {/* Fee */}
-                    <p className="text-gray-600 mt-2">{fee}</p>
-
-                    {/* Tags */}
-                    <div className="mt-4">
-                        {tags.map((tag, index) => (
-                            <span
-                                key={index}
-                                className="inline-block bg-blue-100 text-blue-800 text-xs px-3 py-1 rounded-full mr-2 mb-2"
-                            >
-                                {tag}
-                            </span>
-                        ))}
+                <div className="grid px-6 pt-6 w-full">
+                    <div className='w-full'>
+                        <p className="text-gray-800 text-lg font-bold mb-4">{sportName}</p>
+                        <p className="text-gray-600 mt-2 text-base">
+                            {location} <span className="text-gray-400">({nearestStation})</span>
+                        </p>
+                        <div className="mt-4">
+                            {tags.map((tag, index) => (
+                                <span
+                                    key={index}
+                                    className="inline-block bg-blue-100 text-blue-800 text-sm px-3 py-1 rounded-full mr-2 mb-2"
+                                >
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                        {/* <p>「秋のフットボールフェスティバル」は、2024年10月14日に市民体育館グラウンドで開催される5人制サッカーのトーナメントです。参加費は1チーム10,000円で、オンラインまたは電話で申し込み可能。</p> */}
                     </div>
-                    <div className="px-6 py-4 flex items-center justify-end">
-                        {/* Deadline */}
-                        <p className="text-gray-600 text-sm mr-4">
-                            {`募集締切: ${deadline}`}
-                        </p>
-                        {/* Current Participants */}
-                        <p className="bg-gray-100 text-gray-700 text-xl font-bold px-4 py-2 rounded-lg">
-                            {`現在の参加者: ${currentParticipants}名`}
-                        </p>
+                    <div className='w-full flex justify-end'>
+                        <div className="py-4 flex items-center">
+                            <p className="text-gray-600 text-sm mr-4">
+                                {`募集締切: ${deadline}`}
+                            </p>
+                            <p className="bg-gray-100 text-gray-700 text-xl font-bold px-4 py-2 rounded-lg">
+                                {`現在の参加者: ${currentParticipants}名`}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-
-
-            <hr className="border-t" />
-
-            {/* Join Button */}
-            <div className="px-6 py-4">
-                <button
-                    onClick={onJoinClick}
-                    className="w-full bg-blue-600 text-white text-lg font-semibold py-3 rounded-lg hover:bg-blue-700 transition-colors duration-200"
-                >
-                    参加する
-                </button>
-            </div>
-        </div>
+        </div >
     )
 })
 export default Card
