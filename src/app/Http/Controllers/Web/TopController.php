04e2f6a5b6dc3945043acab2aae89368a0313cd4
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-09-19 08:26:29 +0900

    修正

--- src/app/Http/Controllers/Web/TopController.php
+++ src/app/Http/Controllers/Web/TopController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Services\NotionApi;
 use App\Http\Controllers\Controller;
+use App\Jobs\SendTestEmail;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -11,8 +12,9 @@ class TopController extends Controller
 {
     public function index(): Response
     {
-        $codeCheck = new NotionApi();
-        $codeCheck->getBuildings();
+        SendTestEmail::dispatch("ss", "sssss");
+        // $codeCheck = new NotionApi();
+        // $codeCheck->getBuildings();
         // dd($codeCheck->getBuildings());
         return Inertia::render('Web/Top');
     }
