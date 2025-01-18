01aa8e9c1a8b403d46ccdda6364b771de2e36821
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-01-18 10:07:04 +0900

    修正

--- src/app/Http/Controllers/Web/TopController.php
+++ src/app/Http/Controllers/Web/TopController.php
@@ -11,8 +11,8 @@ class TopController extends Controller
 {
     public function index(): Response
     {
-        $codeCheck = new NotionApi();
-        $codeCheck->getBuildings();
+        // $codeCheck = new NotionApi();
+        // $codeCheck->getBuildings();
         // dd($codeCheck->getBuildings());
         return Inertia::render('Web/Top');
     }
